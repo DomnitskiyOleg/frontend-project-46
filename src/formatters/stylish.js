c8f3775d29f7e1ed5e51c2c66cb4e4314c52f659
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-04-02 16:42:44 +0600

    Change code

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,37 +1,35 @@
 import _ from 'lodash';
 
-const getSpace = (count) => ' '.repeat(count);
+const getSpace = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
 
-const stringify = (data, depth, spaceCount = 4) => {
+const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return String(data);
   }
   const stringEntries = Object.entries(data).map(([key, value]) => {
-    const actualSpaceCount = spaceCount * depth;
-
     if (_.isObject(value)) {
-      return `${getSpace(actualSpaceCount)}${key}: ${stringify(value, depth + 1)}`;
+      return `${getSpace(depth)}  ${key}: ${stringify(value, depth + 1)}`;
     }
-    return `${getSpace(actualSpaceCount)}${key}: ${value}`;
+    return `${getSpace(depth)}  ${key}: ${value}`;
   });
-  return ['{', ...stringEntries, `${getSpace(depth * spaceCount - spaceCount)}}`].join('\n');
+  return ['{', ...stringEntries, `${'    '.repeat(depth - 1)}}`].join('\n');
 };
 
 const getStylishFormat = (diffTree) => {
-  const iter = (node, depth, spaceCount = 4) => {
+  const iter = (node, depth) => {
     if (!_.isArray(node)) {
       const { name, type } = node;
       switch (type) {
         case 'removed':
-          return `${getSpace(spaceCount * depth - 2)}- ${name}: ${stringify(node.value, depth + 1)}`;
+          return `${getSpace(depth)}- ${name}: ${stringify(node.value, depth + 1)}`;
         case 'added':
-          return `${getSpace(spaceCount * depth - 2)}+ ${name}: ${stringify(node.value, depth + 1)}`;
+          return `${getSpace(depth)}+ ${name}: ${stringify(node.value, depth + 1)}`;
         case 'unchanged':
-          return `${getSpace(spaceCount * depth)}${name}: ${stringify(node.value, depth + 1)}`;
+          return `${getSpace(depth)}  ${name}: ${stringify(node.value, depth + 1)}`;
         case 'updated':
           return [
-            `${getSpace(spaceCount * depth - 2)}- ${name}: ${stringify(node.value1, depth + 1)}`,
-            `${getSpace(spaceCount * depth - 2)}+ ${name}: ${stringify(node.value2, depth + 1)}`].join('\n');
+            `${getSpace(depth)}- ${name}: ${stringify(node.value1, depth + 1)}`,
+            `${getSpace(depth)}+ ${name}: ${stringify(node.value2, depth + 1)}`].join('\n');
         default:
           throw new Error(`Unknown type '${type}`);
       }
@@ -39,11 +37,11 @@ const getStylishFormat = (diffTree) => {
     const result = node.map((item) => {
       if (item.type === 'nested') {
         const { children } = item;
-        return `${getSpace(spaceCount * depth)}${item.name}: ${iter(children, depth + 1)}`;
+        return `${getSpace(depth)}  ${item.name}: ${iter(children, depth + 1)}`;
       }
       return iter(item, depth);
     });
-    return ['{', ...result, `${getSpace(depth * spaceCount - spaceCount)}}`].join('\n');
+    return ['{', ...result, `${'    '.repeat(depth - 1)}}`].join('\n');
   };
   return iter(diffTree, 1);
 };
