8354caa0350e1644efd20b47a5a546b5ac421c79
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-04-02 19:03:59 +0600

    Change code

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const getIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
+const getIndent = (depth, corrective = 0, spaceCount = 4) => ' '.repeat(depth * spaceCount + spaceCount + corrective);
 
 const stringify = (data, depth) => {
   if (!_.isObject(data)) {
@@ -8,11 +8,11 @@ const stringify = (data, depth) => {
   }
   const stringEntries = Object.entries(data).map(([key, value]) => {
     if (_.isObject(value)) {
-      return `${getIndent(depth)}  ${key}: ${stringify(value, depth + 1)}`;
+      return `${getIndent(depth)}${key}: ${stringify(value, depth + 1)}`;
     }
-    return `${getIndent(depth)}  ${key}: ${value}`;
+    return `${getIndent(depth)}${key}: ${value}`;
   });
-  return ['{', ...stringEntries, `${'    '.repeat(depth - 1)}}`].join('\n');
+  return ['{', ...stringEntries, `${getIndent(depth, -4)}}`].join('\n');
 };
 
 const getStylishFormat = (diffTree) => {
@@ -21,15 +21,15 @@ const getStylishFormat = (diffTree) => {
       const { name, type } = node;
       switch (type) {
         case 'removed':
-          return `${getIndent(depth)}- ${name}: ${stringify(node.value, depth + 1)}`;
+          return `${getIndent(depth, -2)}- ${name}: ${stringify(node.value, depth + 1)}`;
         case 'added':
-          return `${getIndent(depth)}+ ${name}: ${stringify(node.value, depth + 1)}`;
+          return `${getIndent(depth, -2)}+ ${name}: ${stringify(node.value, depth + 1)}`;
         case 'unchanged':
-          return `${getIndent(depth)}  ${name}: ${stringify(node.value, depth + 1)}`;
+          return `${getIndent(depth)}${name}: ${stringify(node.value, depth + 1)}`;
         case 'updated':
           return [
-            `${getIndent(depth)}- ${name}: ${stringify(node.value1, depth + 1)}`,
-            `${getIndent(depth)}+ ${name}: ${stringify(node.value2, depth + 1)}`].join('\n');
+            `${getIndent(depth, -2)}- ${name}: ${stringify(node.value1, depth + 1)}`,
+            `${getIndent(depth, -2)}+ ${name}: ${stringify(node.value2, depth + 1)}`].join('\n');
         default:
           throw new Error(`Unknown type '${type}`);
       }
@@ -37,13 +37,13 @@ const getStylishFormat = (diffTree) => {
     const result = node.map((item) => {
       if (item.type === 'nested') {
         const { children } = item;
-        return `${getIndent(depth)}  ${item.name}: ${iter(children, depth + 1)}`;
+        return `${getIndent(depth)}${item.name}: ${iter(children, depth + 1)}`;
       }
       return iter(item, depth);
     });
-    return ['{', ...result, `${'    '.repeat(depth - 1)}}`].join('\n');
+    return ['{', ...result, `${getIndent(depth, -4)}}`].join('\n');
   };
-  return iter(diffTree, 1);
+  return iter(diffTree, 0);
 };
 
 export default getStylishFormat;
