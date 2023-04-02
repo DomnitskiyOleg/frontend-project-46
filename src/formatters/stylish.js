aab97e3a137f04831f35299d7ca13e027d06f562
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-04-02 19:07:48 +0600

    Change code

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const getIndent = (depth, corrective = 0, spaceCount = 4) => ' '.repeat(depth * spaceCount + spaceCount + corrective);
+const getIndent = (depth, corrective = 0, spaceCount = 4) => ' '.repeat(depth * spaceCount + spaceCount - corrective);
 
 const stringify = (data, depth) => {
   if (!_.isObject(data)) {
@@ -12,7 +12,7 @@ const stringify = (data, depth) => {
     }
     return `${getIndent(depth)}${key}: ${value}`;
   });
-  return ['{', ...stringEntries, `${getIndent(depth, -4)}}`].join('\n');
+  return ['{', ...stringEntries, `${getIndent(depth, 4)}}`].join('\n');
 };
 
 const getStylishFormat = (diffTree) => {
@@ -21,15 +21,15 @@ const getStylishFormat = (diffTree) => {
       const { name, type } = node;
       switch (type) {
         case 'removed':
-          return `${getIndent(depth, -2)}- ${name}: ${stringify(node.value, depth + 1)}`;
+          return `${getIndent(depth, 2)}- ${name}: ${stringify(node.value, depth + 1)}`;
         case 'added':
-          return `${getIndent(depth, -2)}+ ${name}: ${stringify(node.value, depth + 1)}`;
+          return `${getIndent(depth, 2)}+ ${name}: ${stringify(node.value, depth + 1)}`;
         case 'unchanged':
           return `${getIndent(depth)}${name}: ${stringify(node.value, depth + 1)}`;
         case 'updated':
           return [
-            `${getIndent(depth, -2)}- ${name}: ${stringify(node.value1, depth + 1)}`,
-            `${getIndent(depth, -2)}+ ${name}: ${stringify(node.value2, depth + 1)}`].join('\n');
+            `${getIndent(depth, 2)}- ${name}: ${stringify(node.value1, depth + 1)}`,
+            `${getIndent(depth, 2)}+ ${name}: ${stringify(node.value2, depth + 1)}`].join('\n');
         default:
           throw new Error(`Unknown type '${type}`);
       }
@@ -41,7 +41,7 @@ const getStylishFormat = (diffTree) => {
       }
       return iter(item, depth);
     });
-    return ['{', ...result, `${getIndent(depth, -4)}}`].join('\n');
+    return ['{', ...result, `${getIndent(depth, 4)}}`].join('\n');
   };
   return iter(diffTree, 0);
 };
